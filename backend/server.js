d10ddd64855aeeb54cf291704e426db24efb03fc
kartik2006p <k95241906@gmail.com>
2025-08-11 00:50:04 +0530

    Added Netlify CORS

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// File uploads will be saved in /uploads
+// File uploads will be saved in /uploads folder
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/');
